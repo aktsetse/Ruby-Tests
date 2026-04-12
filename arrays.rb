abac5545d06f8af8346a1b04acfcc4138b63d5f7
Amenuveve Tsetse <ransfordamenuvevetsetse@gmail.com>
2026-04-12 01:26:27 -0500

    Arrays in Ruby

---
+++ arrays.rb
@@ -0,0 +1,5 @@
+# Creating an array
+# Arrays are ordered, integer-indexed collections of any object. They can hold objects of any type, including other arrays.
+
+friends = Array ["Kofi", "Ama", "Kwame", "Esi", "Kojo"]
+puts friends
