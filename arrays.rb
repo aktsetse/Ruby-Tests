d3699a4200a8e84dcb72db56e990e2c797e54c5c
Amenuveve Tsetse <ransfordamenuvevetsetse@gmail.com>
2026-04-12 01:35:15 -0500

    Extra info on arrays

--- arrays.rb
+++ arrays.rb
@@ -1,5 +1,8 @@
 # Creating an array
 # Arrays are ordered, integer-indexed collections of any object. They can hold objects of any type, including other arrays.
+# It even allows you to store data of different types in the same array. You can create an array using square brackets [] or the Array class.
+
+
 
 friends = Array ["Kofi", "Ama", "Kwame", "Esi", "Kojo"]
 puts friends
