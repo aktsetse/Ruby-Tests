4bcd37cebdcd6635c99b9c0b693d829f6652670c
Amenuveve Tsetse <ransfordamenuvevetsetse@gmail.com>
2026-04-12 02:13:52 -0500

    Hashes or Dictionaries in Ruby Language

---
+++ hashes.rb
@@ -0,0 +1,28 @@
+# Hashes stores key value pairs. They are also called dictionaries in other languages. They are similar to arrays but they use keys instead of indices to store values.
+# You can create a hash using curly braces {} or the Hash class. The keys can be of any data type, but they are usually symbols or strings. The values can also be of any data type.
+
+# Creating a hash using curly braces
+person = {
+  "name" => "John",
+  "age" => 30,
+  "city" => "New York"
+}
+
+# Creating a hash using the Hash class
+person = Hash.new
+person["name"] = "John"
+person["age"] = 30
+person["city"] = "New York"
+
+# Accessing values in a hash
+puts person["name"]
+puts person["age"]
+puts person["city"]
+
+# Modifying values in a hash
+person["age"] = 31
+puts person["age"]
+
+# Deleting a key-value pair from a hash
+person.delete("city")
+puts person
