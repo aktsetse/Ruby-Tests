e3cc2f9c91a113fe0ed5471b05b821aeadc057f0
Amenuveve Tsetse <ransfordamenuvevetsetse@gmail.com>
2026-04-12 00:59:39 -0500

    Funny game of Mad Lib

---
+++ mad_lib.rb
@@ -0,0 +1,13 @@
+# Mad Lib is a game where you create a story by filling in the blanks with different types of words.
+
+
+print "Enter a color: "
+color = gets.chomp
+print "Enter a plural noun: "
+plural_noun = gets.chomp
+print "Enter a celebrity: "
+celebrity = gets.chomp
+
+puts "Roses are #{color}"
+puts "#{plural_noun} are blue"
+puts "I love #{celebrity}"
