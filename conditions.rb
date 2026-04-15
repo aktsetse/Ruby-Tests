283bce5c8f874bf36b318a5956ff423fa522305e
Amenuveve Tsetse <ransfordamenuvevetsetse@gmail.com>
2026-04-15 12:45:01 -0500

    more on conditional operators

--- conditions.rb
+++ conditions.rb
@@ -22,3 +22,29 @@ elsif !ismale and istall
 else
     puts "You are not male or not tall or both"
 end
+
+# conditionals cont'd
+# comparisons operators
+# == equal to
+# != not equal to
+# > greater than        
+# < less than
+# >= greater than or equal to
+# <= less than or equal to  
+#   logical operators
+#   and, or, not
+#   and is true if both conditions are true
+#   or is true if at least one condition is true
+#   not is true if the condition is false       
+
+def max(num1, num2, num3)
+  if num1 >= num2 and num1 >= num3
+    return num1
+  elsif num2 >= num1 and num2 >= num3
+    return num2
+  else
+    return num3
+  end
+end
+
+puts max(9,6,4)
